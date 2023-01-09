b1aaff219af7614e2c0a0831f07aaa81428391c9
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-10 01:04:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", "22146428"))
     API_HASH = os.getenv("API_HASH", "a42d8cec8a28746567765dfc47ab558b")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5655172579:AAF4uv3q0lXWEWZZGmF7zpHazV6xNQRGhck")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5837036557:AAG0qjoBBM97NufruZI1yEHdQUrnCDrlKYY")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "mdisksohanbot")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOGsBu5D3sv8I84IjmhjnfhihdDwmx78iU72BCYx_46O9IFSb0VMjez__XTN0f6pfROWMAwwUfKor71cybxseamfhodNN3b4bXhfvg0KrNYhOrxv3G_b0BY4SB5xu7zx1MKEHEq5_Sa6nkJa8s2jrZXc9RbM6Y3_Y2leaVkNkUwNtPPSrpGtkQ71HfIBrGZDke2L5OGKpyU8QkZYxEj-oFQgTMW0gU7QQ2g0rpFWFWXvjSGgvTRhYXcQe-yO1onqRgSeb7iOc-daUWU2scgxhmpESshddXQK6e2fACGKHZqX2OFyl4sSOqSyor3IThXtV40ysF_ZCLPO5NwDHhhwwxwD2F-g=")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001154529469")) 
