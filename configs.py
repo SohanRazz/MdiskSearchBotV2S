3719626b8a3647eddeb4b4b241caa07e63710d41
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-13 15:50:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,11 +5,11 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.getenv("API_ID", "25847347"))
-    API_HASH = os.getenv("API_HASH", "11569826ebbc9f8b14c3ab1c5ddb937b")
+    API_ID = int(os.getenv("API_ID", "21948679"))
+    API_HASH = os.getenv("API_HASH", "d1a9ac6ab9cfa0cefc908280c8a3cca1")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "5863144725:AAEJc5t9RSSNlXVLIRfFu_RasVgFNX3Bm-s")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "mdisksohanbot")
-    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOGsBuwzKEEoiwC5AWQTFgHFEyJPVSxSnjB4hX4zqR7KFcc24lF_AmEa0KSuluBK6z_uZ-gR-sa3r1AE6z-V1ajD1u-Yxi8Rioncnu8jfJ_el0nKJn3Ya_Dsd_4QD-zLQTxF4o47LobOcAoyRa5orrZ0swMxjiqVScSB9jwLfU82bJF4dVAJ7QhipMtb_jDwS__8ZakC1YB7P58cj0rnbvPsKgVIbZ3HYKzh-BkbrRdNQ3YrvBe9msBLW-KIZnaWtQhV11A5yWDJXR-gElAfjgWm1VVT7_8kReGI12HxEDBzSwX6g0mGIYFDttbO8OZE_p1HY7l79lcIom9-ykAicEd1oF60=")
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOJoBu24cYb47wDZ2UeH2cn74ZPnrjoW9jl78UYl96ePDafvUTGuDqKsGwKlpmEt7o3catutEYcIwJMp993NH_1BL2DGFukU7SnAm_p5UT_64DmIY7bCQsTEZT6l4F_fn9_eAkKFG2ULghBQfY0XaZjN1mlTFh24MvqePmbvALhVdd7uQiWBNt0BDdnbqoNkJdH_VcOwmeisCc86n7_yenyWoEhDdZMYpjh5pFlEAhgSoc5I_waxMHIovPSBZ71NhGF3eNjGpzp-gQq3OuLk2eAiouoRLeWJ1tusk001DF4i68xeM6_GfYoQ7R1wG3FGR5sCGmhYS5pQTM7FzMLIeeUtUTZI=")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001836945914")) 
     BOT_USERNAME = os.getenv("BOT_USERNAME", "mdisk_NETFLIX_SEARCH_BOT")
     BOT_OWNER = int(os.getenv("BOT_OWNER", "5897793065"))
