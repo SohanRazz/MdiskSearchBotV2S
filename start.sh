d9923c6c0477f6420d906c3777063942026650f8
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-19 23:59:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/SohanRazz/MdiskSearchBotV2S /MdiskSearchBotV2S
 cd /MdiskSearchBotV2S
-pip3 install -r requirements.txt
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
